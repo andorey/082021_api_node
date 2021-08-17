89a0a868060a96873eab405449dd389a0d49cca3
andoreyrich <andoreyrich@gmail.com>
2021-08-17 19:48:06 +0200

    put and delete requests

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -33,9 +33,9 @@ app.get('/cities', function (req, res){
     res.send(cities)
 })
 
-app.get('/cities/:id', function (req, res){
+app.get('/cities/:id', function (req, res){     // replaced empty url with buttons
     try{
-        let city = cities.find( el => el.id === Number( req.params.id ) );
+        const city = cities.find( el => el.id === Number( req.params.id ) );
         res.send(city.name ? city : '<button style="color: white; background-color: seagreen">empty</button>')
     }catch{
         res.send('<button style="color: white; background-color: indianred">the end object</button>')
@@ -43,13 +43,24 @@ app.get('/cities/:id', function (req, res){
 })
 
 app.post('/cities', function (req, res){
-    let out = {
-        id: cities.slice(-1)[0].id + 1,     //id: cities[cities.length-1].id + 1
+    const city = {
+        id: cities.slice(-1)[0].id + 1,             //id: cities[cities.length-1].id + 1
         name: req.body.name
     }
-    cities.push(out)
+    cities.push(city)
     console.log(req.body)
-    res.send(out);
+    res.send(city);
+})
+
+app.put('/cities/:id', function (req, res){         // replace element's name
+    const city = cities.find(el => el.id === Number(req.params.id))
+    city.name = req.body.name;
+    res.sendStatus(200)
+})
+
+app.delete('/cities/:id', function (req, res){      // remove item from list
+    cities = cities.filter(el => el.id !== Number(req.params.id))
+    res.sendStatus(200)
 })
 
 app.listen(3008, function (){
