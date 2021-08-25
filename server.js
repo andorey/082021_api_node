f5bf8121fd558bfcd1a95a0b2bdecb986fb282a3
andoreyrich <andoreyrich@gmail.com>
2021-08-25 12:33:18 +0200

    started database with mongodb

--- server.js
+++ server.js
@@ -1,78 +1,78 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
+let db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
-let cities = [
-    {
-        id:1,
-        name: 'London'
-    },
-    {
-        id:2,
-        name: 'Warsaw'
-    },
-    {
-        id:3,
-        name:'Philadelphia'
-    },
-    {
-        id:4,
-        name:'Hamilton'
-    }
-]
+// let cities = [
+//     {
+//         id:1,
+//         name: 'London'
+//     },
+//     {
+//         id:2,
+//         name: 'Warsaw'
+//     },
+//     {
+//         id:3,
+//         name:'Philadelphia'
+//     },
+//     {
+//         id:4,
+//         name:'Hamilton'
+//     }
+// ]
 
 app.get('/', function(req, res){
     res.send('you on my API')
 })
 
 app.get('/cities', function (req, res){
-    res.send(cities)
+    db.collection('cities').find().toArray((err, docs) => {
+        if (err) throw err;
+        res.send(docs)
+    })
 })
 
-app.get('/cities/:id', function (req, res){     // replaced empty url with buttons
-    try{
-        const city = cities.find( el => el.id === Number( req.params.id ) );
-        res.send(city.name ? city : '<button style="color: white; background-color: seagreen">empty</button>')
-    }catch{
-        res.send('<button style="color: white; background-color: indianred">the end object</button>')
-    }
-})
-
-app.post('/cities', function (req, res){            // adding new elements to list
-    const city = {
-        id: cities.slice(-1)[0].id + 1,             //id: cities[cities.length-1].id + 1
-        name: req.body.name
-    }
-    cities.push(city)
-    console.log(req.body)
-    res.send(city);
-})
-
-app.put('/cities/:id', function (req, res){         // replace the name and id of the element where id had priority
-    cities.map(el => el.id === Number(req.body.id || req.params.id) ? el.name = req.body.name : '')
-    res.sendStatus(200)
-})
-
-// app.put('/cities/:id', function (req, res){         // replace element's name
-//     const city = cities.find(el => el.id === Number(req.params.id))
-//     city.name = req.body.name;
+// app.get('/cities/:id', function (req, res){     // replaced empty url with buttons
+//     try{
+//         const city = cities.find( el => el.id === Number( req.params.id ) );
+//         res.send(city.name ? city : '<button style="color: white; background-color: seagreen">empty</button>')
+//     }catch{
+//         res.send('<button style="color: white; background-color: indianred">the end object</button>')
+//     }
+// })
+//
+// app.post('/cities', function (req, res){            // adding new elements to list
+//     const city = {
+//         id: cities.slice(-1)[0].id + 1,             //id: cities[cities.length-1].id + 1
+//         name: req.body.name
+//     }
+//     cities.push(city)
+//     console.log(req.body)
+//     res.send(city);
+// })
+//
+// app.put('/cities/:id', function (req, res){         // replace the name and id of the element where id had priority
+//     cities.map(el => el.id === Number(req.body.id || req.params.id) ? el.name = req.body.name : '')
+//     res.sendStatus(200)
+// })
+//
+// app.delete('/cities/:id', function (req, res){      // remove item from list with req.params.id
+//     cities = cities.filter(el => el.id !== Number(req.params.id))
 //     res.sendStatus(200)
 // })
 
-// app.delete('/cities', function (req, res){      // remove item from list with req.body.id
-//     cities = cities.filter(el => el.id !== Number(req.body.id))
-//     res.sendStatus(200)
-// })
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) throw err
+    db = client.db('myapi');
 
-app.delete('/cities/:id', function (req, res){      // remove item from list with req.params.id
-    cities = cities.filter(el => el.id !== Number(req.params.id))
-    res.sendStatus(200)
-})
-
-app.listen(3008, function (){
-    console.log('API started')
+    app.listen(3008, function (){
+        console.log('API started with db')
+    })
 })
